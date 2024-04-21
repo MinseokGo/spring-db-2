7f4cac493288abd42bc7dbd873e5aeff9a84a619
MinseokGo <rhalstjr1999@naver.com>
2024-04-21 20:01:48 +0900

    feat: named jdbc template v3

---
+++ src/main/java/hello/itemservice/config/JdbcTemplateV3Config.java
@@ -0,0 +1,42 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV2;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcTemplateV3Config {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV2(namedParameterJdbcTemplate());
+    }
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public SimpleJdbcInsert simpleJdbcInsert() {
+        return new SimpleJdbcInsert(dataSource)
+                .withTableName("item")
+                .usingGeneratedKeyColumns("id")
+                .usingColumns("item_name", "price", "quantity");    // 생략 가능
+    }
+}
---
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV3.java
@@ -0,0 +1,93 @@
+package hello.itemservice.repository.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCondition;
+import hello.itemservice.repository.ItemUpdateDto;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.util.StringUtils;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JdbcTemplateItemRepositoryV3 implements ItemRepository {
+
+    private final SimpleJdbcInsert simpleJdbcInsert;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Override
+    public Item save(Item item) {
+        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(item);
+        simpleJdbcInsert.executeAndReturnKey(sqlParameterSource );
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        String sql = "update item "
+                + "set item_name=:itemName, price=:price, quantity=:quantity "
+                + "where id=:id";
+
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+                .addValue("itemName", updateParam.itemName())
+                .addValue("price", updateParam.price())
+                .addValue("quantity", updateParam.quantity())
+                .addValue("id", itemId);
+        jdbcTemplate.update(sql, sqlParameterSource);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        String sql = "select id, item_name, price, quantity from item where id = :id";
+        try {
+            Map<String, Object> paramMap = Map.of("id", id);
+            Item item = jdbcTemplate.queryForObject(sql, paramMap, itemRowMapper());
+            return Optional.of(Objects.requireNonNull(item));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCondition condition) {
+        String itemName = condition.itemName();
+        Integer maxPrice = condition.maxPrice();
+
+        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(condition);
+
+        String sql = "select id, item_name, price, quantity from item";
+        if (StringUtils.hasText(itemName) || maxPrice != null) {
+            sql += " where";
+        }
+
+        boolean andFlag = false;
+        if (StringUtils.hasText(itemName)) {
+            sql += " item_name like concat('%',:itemName,'%')";
+            andFlag = true;
+        }
+        if (maxPrice != null) {
+            if (andFlag) {
+                sql += " and";
+            }
+            sql += " price <= :maxPrice";
+        }
+        log.info("sql={}", sql);
+
+        return jdbcTemplate.query(sql, sqlParameterSource, itemRowMapper());
+    }
+    private RowMapper<Item> itemRowMapper() {
+        return BeanPropertyRowMapper.newInstance(Item.class);
+    }
+}
