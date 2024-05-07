bb4449870b180b02d0554b11ff77fa53038bb9a9
MinseokGo <rhalstjr1999@naver.com>
2024-05-07 19:12:05 +0900

    refactor: data source bean

---
+++ src/main/java/hello/itemservice/config/JdbcTemplateV2Config.java
@@ -0,0 +1,39 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV2;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcTemplateV2Config {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        return new DriverManagerDataSource();
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV2(namedParameterJdbcTemplate());
+    }
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+}
