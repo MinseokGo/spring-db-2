70d270c6c1373d47ae204d2c92fb4af47b6217d3
MinseokGo <rhalstjr1999@naver.com>
2024-06-07 18:59:01 +0900

    test: inner and outer transaction

--- src/test/java/hello/springtransaction/propagation/BasicTransactionTest.java
+++ src/test/java/hello/springtransaction/propagation/BasicTransactionTest.java
@@ -1,5 +1,7 @@
 package hello.springtransaction.propagation;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 @Slf4j
@@ -78,7 +81,36 @@ class BasicTransactionTest {
         transactionManager.commit(inner);   // 내부 트랜잭션에서는 커밋을 해도 동작을 하지 않는다.
 
         log.info("외부 트랜잭션 커밋");
-        transactionManager.commit(outer);   // 내부 트랜잭션에서 롤백 시, 트랜잭션이 마킹되어 해당 트랜잭션을 재사용할 수 없게 된다.
+        transactionManager.commit(outer);
+    }
+
+    @Test
+    void outer_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = transactionManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = transactionManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 커밋");
+        transactionManager.commit(inner);
+
+        log.info("외부 트랜잭션 롤백");
+        transactionManager.rollback(outer);
+    }
+
+    @Test
+    void inner_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = transactionManager.getTransaction(new DefaultTransactionAttribute());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = transactionManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 롤백");
+        transactionManager.rollback(inner); // rollback-only mark(표시), 이후 커밋 불가
+
+        log.info("외부 트랜잭션 커밋");
+        assertThatThrownBy(() -> transactionManager.commit(outer))
+                .isInstanceOf(UnexpectedRollbackException.class);
     }
 
     @TestConfiguration
